b40fdf503a0a899467f6ec184569ca5b1f7ad73a
XiaoBingBy <XiaoBingBy@Gmail.com>
2020-04-21 23:10:51 +0800

    magical value Cleanup

--- spring-core/src/main/java/org/springframework/core/io/support/PathMatchingResourcePatternResolver.java
+++ spring-core/src/main/java/org/springframework/core/io/support/PathMatchingResourcePatternResolver.java
@@ -291,7 +291,7 @@ public class PathMatchingResourcePatternResolver implements ResourcePatternResol
 		else {
 			// Generally only look for a pattern after a prefix here,
 			// and on Tomcat only after the "*/" separator for its "war:" protocol.
-			int prefixEnd = (locationPattern.startsWith("war:") ? locationPattern.indexOf("*/") + 1 :
+			int prefixEnd = (locationPattern.startsWith(ResourceUtils.WAR_URL_PREFIX) ? locationPattern.indexOf(ResourceUtils.WAR_URL_SEPARATOR) + 1 :
 					locationPattern.indexOf(':') + 1);
 			if (getPathMatcher().isPattern(locationPattern.substring(prefixEnd))) {
 				// a file pattern
